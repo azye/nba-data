d21c6e10c043ea26484e23101b53175e8f19420e
Alex Ye <alexzye1@gmail.com>
2017-06-11 15:48:52 -0700

    Final submission

--- Per48.scala
+++ Per48.scala
@@ -1,4 +1,3 @@
-package project
 import scala.io._
 import org.apache.log4j._
 import org.apache.spark.{ SparkConf, SparkContext }
@@ -6,31 +5,29 @@ import org.apache.spark.rdd.RDD.rddToPairRDDFunctions
 
 /** Players.txt column names
  * Player,Pos,Age,Team,GP,GS,MIN,FGM,FGA,3PM,3PA,2PM,2PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF,PTS,Year
- * 
+ *
  */
-object Per48 {
-  
+object per48 {
   /**
    * Returns the players per 48 minutes of the given stat over all their years of play
-   * 
+   *
    * statColumn - use the number below one of the []ed stats
    * PlayerName - name of the player
    * sc					- the SparkContext
    * can use any per48 method on any of the stats in []
    * Player,Pos,Age,Team,GP,GS,MIN,[FGM,FGA,3PM,3PA,2PM,2PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF,PTS],Year
    *                                [ 7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22]
-   * 
+   *
    */
-  def per48(statColumn: Int, PlayerName: String, sc: SparkContext) = {
+  def per48(statColumn: Int, PlayerName: String) = {
     var yearColumn = 23
     val minutesColumn = 6
-    
-    
-    var players = sc.textFile("Players.txt")
-    
+
+    var players = sc.textFile("data/players.txt")
+
     var stats = players.map(line => (line.split(",")(0).toUpperCase(),
       (line.split(",")(yearColumn).toInt, line.split(",")(statColumn).toInt, line.split(",")(minutesColumn).toDouble / 48)))
-    
+
     .filter(_._1 == PlayerName.toUpperCase())
     .map({case (name, (year, stat, minutes)) => (year, (stat, minutes))})
     .mapValues(f => {
@@ -39,26 +36,26 @@ object Per48 {
      else
        f._1 / f._2
     })
-    
+
     .collect().sortBy(_._1).foreach(println(_))
-    
-  } 
+
+  }
   /**
    * Returns the Best Player for a given stat each year
-   * 
+   *
    * statColumn - use the number below one of the []ed stats
    * sc					- the SparkContext
    * can use any per48 method on any of the stats in []
    * Player,Pos,Age,Team,GP,GS,MIN,[FGM,FGA,3PM,3PA,2PM,2PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF,PTS],Year
    *                                [ 7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22]
-   * 
+   *
    */
-  def bestPer48(statColumn: Int, sc: SparkContext) = {
+  def bestPer48(statColumn: Int) = {
     var yearColumn = 23
     val minutesColumn = 6
-    
-    var players = sc.textFile("Players.txt")
-    
+
+    var players = sc.textFile("data/players.txt")
+
     var stats = players.map(line => (line.split(",")(0),
       (line.split(",")(yearColumn).toInt, line.split(",")(statColumn).toInt, line.split(",")(minutesColumn).toDouble)))
     .filter(_._2._3 > 60)
@@ -72,37 +69,37 @@ object Per48 {
     .groupByKey()
     .map(f => (f._1, (f._2.maxBy(_._2))))
     .collect().sortBy(_._1).foreach(println(_))
-    
+
   }
-  
+
   /**
    * Returns the teams overall per 48 for a given stat
-   * 
+   *
    * statColumn - use the number below one of the []ed stats
    * team			  - abbreviation of the team name
    * sc					- the SparkContext
    * can use any per48 method on any of the stats in []
    * Player,Pos,Age,Team,GP,GS,MIN,[FGM,FGA,3PM,3PA,2PM,2PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF,PTS],Year
    *                                [ 7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22]
-   * 
+   *
    */
-  def teamPer48(statColumn: Int, team: String, sc: SparkContext) = {
+  def teamPer48(statColumn: Int, team: String) = {
     val yearColumn = 23
     val minutesColumn = 6
-    var players = sc.textFile("Players.txt")
+    var players = sc.textFile("data/players.txt")
     var stats = players.map(line => (line.split(",")(3),
       (line.split(",")(yearColumn).toInt, line.split(",")(statColumn).toInt, line.split(",")(minutesColumn).toDouble / 48)))
     .filter(_._1 == team)
     .map({case (team, (year, stat, min)) => (team + ": " + year, (stat / min))})
-    .combineByKey(x => x, 
-        (acc: (Double), z) => (acc + z), 
+    .combineByKey(x => x,
+        (acc: (Double), z) => (acc + z),
         (acc1: (Double), acc2: (Double)) => (acc1 + acc2))
     .mapValues(f => f)
     .map({ case (teamYear, stat) => (teamYear.split(": ")(1), teamYear.split(": ")(0), stat)})
     .collect().sortBy(_._1).foreach(println(_))
-    
+
   }
-  
+
   /**
    * Returns the Championship Teams per 48 for a given stat for each year
    * statColumn - use the number below one of the []ed stats
@@ -110,32 +107,32 @@ object Per48 {
    * can use any per48 method on any of the stats in []
    * Player,Pos,Age,Team,GP,GS,MIN,[FGM,FGA,3PM,3PA,2PM,2PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF,PTS],Year
    *                                [ 7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22]
-   * 
+   *
    */
-  def championTeamPer48(statColumn: Int, sc: SparkContext) = {
-    var champions = sc.textFile("champions.txt")
-    var players = sc.textFile("Players.txt")
-    
+  def championTeamPer48(statColumn: Int) = {
+    var champions = sc.textFile("data/champions.txt")
+    var players = sc.textFile("data/players.txt")
+
     var yearColumn = 23
     val minutesColumn = 6
-    
+
     var stats = players.map(line => (line.split(",")(3),
       (line.split(",")(yearColumn).toInt, line.split(",")(statColumn).toInt, line.split(",")(minutesColumn).toDouble / 48)))
     .map({case (team, (year, stat, min)) => (team + ": " + year, (stat / min))})
-    .combineByKey(x => x, 
-        (acc: (Double), z) => (acc + z), 
+    .combineByKey(x => x,
+        (acc: (Double), z) => (acc + z),
         (acc1: (Double), acc2: (Double)) => (acc1 + acc2))
     .mapValues(f => f)
-    
+
     var nameYear = champions.map(line => (line.split(",")(0), line.split(",")(1).toInt))
     .map({ case (name, year) => (name + ": " + year, 0.0)})
-   
+
     nameYear.join(stats)
     .mapValues(f => f._2)
     .map({ case (teamYear, stat) => (teamYear.split(": ")(1), teamYear.split(": ")(0), stat)})
     .collect().sortBy(_._1).foreach(println(_))
   }
-  
+
   /**
    * Returns the MVP Player's per48 stat each year
    * statColumn - use the number below one of the []ed stats
@@ -143,14 +140,14 @@ object Per48 {
    * can use any per48 method on any of the stats in []
    * Player,Pos,Age,Team,GP,GS,MIN,[FGM,FGA,3PM,3PA,2PM,2PA,FTM,FTA,ORB,DRB,AST,STL,BLK,TOV,PF,PTS],Year
    *                                [ 7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22]
-   * 
+   *
    */
-  def mvpPer48(statColumn: Int, sc: SparkContext) = {
-    var mvpPlayers = sc.textFile("mvps.txt")
-    var players = sc.textFile("Players.txt")
+  def mvpPer48(statColumn: Int) = {
+    var mvpPlayers = sc.textFile("data/mvps.txt")
+    var players = sc.textFile("data/players.txt")
     var yearColumn = 23
     val minutesColumn = 6
-    
+
     var stats = players.map(line => (line.split(",")(0).toLowerCase(),
       (line.split(",")(yearColumn).toInt, line.split(",")(statColumn).toInt, line.split(",")(minutesColumn).toDouble / 48)))
     .map({case (name, (year, stat, minutes)) => (name + ": " + year, (stat, minutes))})
@@ -160,14 +157,14 @@ object Per48 {
      else
        f._1 / f._2
     })
-      
+
     var nameYear = mvpPlayers.map(line => (line.split(",")(0).toLowerCase(), line.split(",")(1).toInt))
     .map({ case (name, year) => (name + ": " + year, 0.0)})
-    
+
     nameYear.join(stats)
     .mapValues(f => f._2)
     .map({ case (nameYear, stat) => (nameYear.split(": ")(1), nameYear.split(": ")(0), stat)})
     .collect().sortBy(_._1).foreach(println(_))
   }
-    
+
 }
